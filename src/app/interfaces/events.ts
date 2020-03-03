c7152f2e34cdb090c5027f812c42344d685e6a67
lprivette <lprivette@usgs.gov>
2020-03-03 17:58:42 -0500

    added files

---
+++ src/app/interfaces/events.ts
@@ -0,0 +1,12 @@
+export interface Events {
+    event_id: number;
+    event_name: string;
+    event_start_date: string;
+    event_end_date: string;
+    event_description: string;
+    event_type_id: number;
+    event_status_id: number;
+    event_coordinator: number;
+    instruments: any;
+    hwms: any;
+}
