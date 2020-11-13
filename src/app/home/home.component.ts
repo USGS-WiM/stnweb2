8ae7d8098a66fb7a4b3363af164fd675a3d654e2
09Kathy <kathymdooley@gmail.com>
2020-11-13 13:39:54 -0600

    Issue #65 - units to miles

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -228,6 +228,8 @@ export class HomeComponent implements OnInit {
 
         const drawnItems = L.featureGroup().addTo(this.map);
 
+        //User can select from drawing a line or polygon; other options are disabled
+        //Measurements are in miles
         const drawControl = new L.Control.Draw({
             edit: {
                 featureGroup: drawnItems,
@@ -239,52 +241,36 @@ export class HomeComponent implements OnInit {
                 polygon: {
                     allowIntersection: false,
                     showArea: true,
+                    metric: false,
                 },
                 marker: false,
                 circle: false,
                 circlemarker: false,
                 rectangle: false,
+                polyline: {
+                    metric: false,
+                    feet: false,
+                },
             },
         });
 
+        //Add the buttons to the map
         this.map.addControl(drawControl);
 
         // Truncate value based on number of decimals
         const _round = function (num, len) {
             return Math.round(num * Math.pow(10, len)) / Math.pow(10, len);
         };
-        // Helper method to format LatLng object (x.xxxxxx, y.yyyyyy)
-        const strLatLng = function (latlng) {
-            return (
-                '(' + _round(latlng.lat, 6) + ', ' + _round(latlng.lng, 6) + ')'
-            );
-        };
 
         // Generate popup content based on layer type
         // - Returns HTML string, or null if unknown object
         const getPopupContent = function (layer) {
-            // Marker - add lat/long
-            if (layer instanceof L.Marker || layer instanceof L.CircleMarker) {
-                return strLatLng(layer.getLatLng());
-                // Circle - lat/long, radius
-            } else if (layer instanceof L.Circle) {
-                const center = layer.getLatLng(),
-                    radius = layer.getRadius();
-                return (
-                    'Center: ' +
-                    strLatLng(center) +
-                    '<br />' +
-                    'Radius: ' +
-                    _round(radius, 2) +
-                    ' m'
-                );
-                // Rectangle/Polygon - area
-            } else if (layer instanceof L.Polygon) {
+            if (layer instanceof L.Polygon) {
                 const latlngs = layer._defaultShape
                         ? layer._defaultShape()
                         : layer.getLatLngs(),
                     area = L.GeometryUtil.geodesicArea(latlngs);
-                return 'Area: ' + L.GeometryUtil.readableArea(area, true);
+                return 'Area: ' + L.GeometryUtil.readableArea(area);
                 // Polyline - distance
             } else if (layer instanceof L.Polyline) {
                 const latlngs = layer._defaultShape
@@ -297,7 +283,8 @@ export class HomeComponent implements OnInit {
                     for (let i = 0; i < latlngs.length - 1; i++) {
                         distance += latlngs[i].distanceTo(latlngs[i + 1]);
                     }
-                    return 'Distance: ' + _round(distance, 2) + ' m';
+                    distance = distance * 0.000621371;
+                    return 'Distance: ' + _round(distance, 2) + ' mi';
                 }
             }
             return null;
