8b9e5e3f1f5ff425b9c621f2a958e09a99afd1ef
blakedraper <bdraper@usgs.gov>
2020-11-19 08:47:00 -0600

    finish merge

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -373,8 +373,6 @@ export class HomeComponent implements OnInit {
         return event && event.event_name ? event.event_name : '';
     }
 
-<<<<<<< HEAD
-=======
     //Match what user is typing to the index of the corresponding event
     //Not case sensative
     private _filter(event_name: string): Event[] {
@@ -399,52 +397,6 @@ export class HomeComponent implements OnInit {
         return sensor && sensor.sensor ? sensor.sensor : '';
     }
 
-    scaleLookup(mapZoom) {
-        switch (mapZoom) {
-            case 19:
-                return '1,128';
-            case 18:
-                return '2,256';
-            case 17:
-                return '4,513';
-            case 16:
-                return '9,027';
-            case 15:
-                return '18,055';
-            case 14:
-                return '36,111';
-            case 13:
-                return '72,223';
-            case 12:
-                return '144,447';
-            case 11:
-                return '288,895';
-            case 10:
-                return '577,790';
-            case 9:
-                return '1,155,581';
-            case 8:
-                return '2,311,162';
-            case 7:
-                return '4,622,324';
-            case 6:
-                return '9,244,649';
-            case 5:
-                return '18,489,298';
-            case 4:
-                return '36,978,596';
-            case 3:
-                return '73,957,193';
-            case 2:
-                return '147,914,387';
-            case 1:
-                return '295,828,775';
-            case 0:
-                return '591,657,550';
-        }
-    }
-
->>>>>>> 89df7019fd3613debc566b362fab52339923724c
     // another method to get event sites
     /* getEventSites(arr, arr2) {
     const ret = [];
