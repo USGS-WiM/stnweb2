099cc1d26a5da2840bb7d91d9e4de1773260edf8
Myers <EMYERS@USGS.GOV>
2020-12-15 09:36:48 -0600

    clean up

--- src/app/map/map.component.ts
+++ src/app/map/map.component.ts
@@ -237,18 +237,7 @@ export class MapComponent implements OnInit {
             //Get id and name of most recent event
             this.currentEvent = this.events[0].event_id;
             this.currentEventName = this.events[0].event_name;
-            // TODO: set up subject to track the next current event and move
-            //this.eventSites.next(this.currentEvent)
 
-            /* this.filteredEvents$ = this.eventsControl.valueChanges.pipe(
-                debounceTime(200),
-                distinctUntilChanged(),
-                switchMap((searchTerm) =>
-                    APP_UTILITIES.FILTER_EVENT(searchTerm, this.events)
-                )
-            ); */
-
-            // allow user to type into the event selector to view matching events
             // allow user to type into the event selector to view matching events
             this.filteredEvents$ = this.mapFilterForm
                 .get('eventsControl')
