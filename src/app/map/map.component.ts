cf60dc8f5df68e8d1ccff6bd9965287f15cbf87c
09Kathy <kathymdooley@gmail.com>
2021-01-19 12:39:23 -0600

    again

--- src/app/map/map.component.ts
+++ src/app/map/map.component.ts
@@ -309,7 +309,7 @@ export class MapComponent implements OnInit {
                 //when 'clear filters' button is clicked, surVal is null, which would cause surVal[x] to trigger a console error
                 if (surVal !== null) {
                     //if the Surveyed button was selected, and user pressed the Not Surveyed button,
-                    //turn off the Surveyed button and set url survey param to false
+                    // turn off the Surveyed button and set url survey param to false
                     if (surVal[0] === 'true' && surVal[1] === 'false') {
                         this.mapFilterForm
                             .get('surveyedControl')
