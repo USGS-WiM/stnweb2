ca479b7ad8f0fcb8de80a9bdcbb7446afac658f6
lprivette <lprivette@usgs.gov>
2021-11-15 20:57:55 -0500

    WIP: adds table with users, router link

--- src/app/user-management/user-management.component.ts
+++ src/app/user-management/user-management.component.ts
@@ -1,15 +1,46 @@
 import { Component, OnInit } from '@angular/core';
-
+import { UserService } from '@services/user.service';
+import { MatTableDataSource } from '@angular/material/table';
 @Component({
   selector: 'app-user-management',
   templateUrl: './user-management.component.html',
   styleUrls: ['./user-management.component.scss']
 })
 export class UserManagementComponent implements OnInit {
+  userDataSource = new MatTableDataSource([]);
+  users = [];
+  isLoading = true;
+  displayedColumns: string[] = [
+    'Username',
+    'Last Name',
+    'First Name',
+    'Email',
+    'Agency',
+    'Role'
+  ];
 
-  constructor() { }
+  constructor(
+    private userService: UserService,
+  ) { }
 
   ngOnInit(): void {
+    this.getSiteSensorData()
+  }
+
+  createDataSource(data) {
+    
+
+  }
+  getSiteSensorData() {
+    this.userService
+      .getAllUsers()
+      .subscribe((results) => {
+        this.isLoading = false;
+        this.users = results;
+        this.userDataSource.data = this.users;
+        console.log(this.users)
+      },
+      error => this.isLoading = false);
   }
 
 }
