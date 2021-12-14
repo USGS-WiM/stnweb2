338a4606acde5330cb16c31482e7dd20ed764664
Gleason <agleason@contractor.usgs.gov>
2021-12-14 08:32:52 -0600

    #281 - change Reference Marks to Reference Datums

--- src/app/sensor-dialog/sensor-dialog.component.ts
+++ src/app/sensor-dialog/sensor-dialog.component.ts
@@ -48,7 +48,7 @@ export class SensorDialogComponent implements OnInit {
   ];
 
   displayedTapedownColumns: string[] = [
-    'ReferenceMark',
+    'ReferenceDatum',
     'Elevation',
     'OffsetCorrection',
     'WaterSurface',
@@ -157,7 +157,7 @@ export class SensorDialogComponent implements OnInit {
             }
             tapedownArray[0].water_surface = results[0].water_surface;
             tapedownArray[0].offset_correction = results[0].offset_correction;
-            // get reference mark info using objective_point_id
+            // get reference datum info using objective_point_id
             self.siteService
             .getOPInfo(results[0].objective_point_id)
             .subscribe((objectivePoints) => {
